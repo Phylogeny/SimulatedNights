5976011c2fd04bca483761555f9e7412a7789bbb
Phylogeny <molecularphylo@gmail.com>
2017-07-18 05:12:54 -0700

    Restricted allowed MC version to 1.12+.

--- src/main/java/com/phylogeny/simulatednights/reference/Reference.java
+++ src/main/java/com/phylogeny/simulatednights/reference/Reference.java
@@ -7,7 +7,7 @@ public class Reference
 	public static final String MOD_PATH = "com.phylogeny." + MOD_ID;
 	public static final String VERSION = "@VERSION@";
 	public static final String UPDATE_JSON = "@UPDATE@";
-	public static final String MC_VERSIONS_ACCEPTED = "[1.9.4,)";
+	public static final String MC_VERSIONS_ACCEPTED = "[1.12,)";
 	public static final String CLIENT_CLASSPATH = MOD_PATH + ".proxy.ProxyClient";
 	public static final String COMMON_CLASSPATH = MOD_PATH + ".proxy.ProxyCommon";
 	public static final String GUI_FACTORY_CLASSPATH = MOD_PATH + ".client.config.GuiFactorySimulatedNights";
